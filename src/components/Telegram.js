9f64af912e6e8aa54033fd1d582820600adbf8d7
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-23 20:14:53 +0200

    add thread header

--- src/components/Telegram.js
+++ src/components/Telegram.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import SideBar from './SideBar'
+import Thread from './Thread'
+import './Telegram.css'
 
 const Telegram = () => {
     return (
         <div className="telegram">
             <SideBar />
+            <Thread />
         </div>
     )
 }
---
+++ src/components/Thread.js
@@ -0,0 +1,25 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import { MoreHoriz } from '@material-ui/icons'
+import React from 'react'
+import './Thread.css'
+
+const Thread = () => {
+    return (
+        <div className="thread">
+            <div className="thread__header">
+                <div className="thread__header__contents">
+                    <Avatar />
+                    <div className="thread__header__contents__info">
+                        <h4>Threadname</h4>
+                        <h5>Last seen</h5>
+                    </div>
+                </div>
+                <IconButton>
+                    <MoreHoriz className="thread__header__details" />
+                </IconButton>
+            </div>
+        </div>
+    )
+}
+
+export default Thread
