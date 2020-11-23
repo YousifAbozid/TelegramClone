2e5ef70a829c9bb3ea2d6e44fb72766d21bf1ec9
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-23 04:22:39 +0200

    add some styles

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import SearchIcon from "@material-ui/icons/Search"
+import './SideBar.css'
+import BorderColorOutlinedIcon from '@material-ui/icons/BorderColorOutlined'
+import { IconButton } from '@material-ui/core'
 
 const SideBar = () => {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
                 <div className="sidebar__search">
+                    <SearchIcon className="sidebar__searchIcon" />
                     <input placeholder="Search" className="sidebar__input" />
                 </div>
+                <IconButton variant="outlined" id="sidebar__button" >
+                    <BorderColorOutlinedIcon />
+                </IconButton>
             </div>
+            <div className="sidebar__thread"></div>
+            <div className="sidebar__bottom"></div>
         </div>
     )
 }
