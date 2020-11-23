670e5e302f110fba864850ae76c983bd0f5e270d
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-23 23:01:39 +0200

    add login component and it's styles

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
+import { auth, provider } from '../../firebase'
+import './Login.css'
 
 const Login = () => {
+    const signIn = () => {
+        auth.signInWithPopup(provider)
+            .catch((error) => {
+                alert(error.message)
+            })
+    }
+    
     return (
-        <div>
-            login
+        <div className="login">
+            <div className="login__telegram">
+                <img />
+                <h1>Telegram</h1>
+            </div>
+            <Button onClick={signIn}>Sign in</Button>
         </div>
     )
 }
