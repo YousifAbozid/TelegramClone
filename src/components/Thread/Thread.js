23562876ae5da4042e59efef1896ce004f577681
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-24 02:12:49 +0200

    add message component and sending messages has bugs right now

--- src/components/Thread/Thread.js
+++ src/components/Thread/Thread.js
@@ -1,13 +1,50 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Avatar, IconButton } from '@material-ui/core'
 import { MoreHoriz, SendRounded, TimerOutlined, MicNoneOutlined } from '@material-ui/icons'
 import './Thread.css'
+import firebase from 'firebase'
+import database from '../../firebase'
+import { useSelector } from 'react-redux'
+import { selectThreadId, selectThreadName } from '../../features/threadSlice'
+import { selectUser } from '../../features/userSlice'
+import Message from '../Message/Message'
 
 const Thread = () => {
     const [input, setInput] = useState('')
+    const [messages, setMessages] = useState([])
+    const user = useSelector(selectUser)
+    const threadName = useSelector(selectThreadName)
+    const threadId = useSelector(selectThreadId)
+
+    useEffect(() => {
+        if (threadId) {
+            database
+                .collection('threads')
+                .collection('messages')
+                .orderBy('timestamp', 'desc')
+                .onSnapShot((snapshot) => setMessages(snapshot.docs.map((doc) => {
+                    return {
+                        id: doc.id,
+                        data: doc.data
+                    }
+                })))
+        }
+    }, [])
 
     const sendMessage = (event) => {
         event.preventDefault()
+        database
+            .collection('threads')
+            .doc(threadId) // this not working now in 3:35 min, I know where is the bug and they don't ha
+            .collection('messages')
+            .add({
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                message: input,
+                uid: user.uid,
+                photo: user.photo,
+                email: user.email,
+                displayName: user.displayName
+            })
 
         setInput('')
     }
@@ -26,7 +63,11 @@ const Thread = () => {
                     <MoreHoriz className="thread__header__details" />
                 </IconButton>
             </div>
-            <div className="thread__messages">messages</div>
+            <div className="thread__messages">
+                {messages.map(({ id, data }) => (
+                    <Message key={id} data={data} />
+                ))}
+            </div>
             <div className="thread__input">
                 <form>
                     <input
